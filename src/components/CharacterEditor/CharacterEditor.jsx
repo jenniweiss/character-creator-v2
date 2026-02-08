961d130117e5a544f9e23a967fe4182ff65b81c3
Jennifer Weiss <weissj@mit.edu>
2026-02-07 20:52:01 -0500

    my changes

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspective}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -78,6 +79,7 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
+       
       <div className={styles.characterWrapper}>
         <Character
           body={body}
@@ -88,6 +90,7 @@ function App() {
           clothesColor={clothesColor}
         />
       </div>
+      
     </main>
   );
 }
